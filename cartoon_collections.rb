b555125a3764243107a83186de12ffb402cd368a
Kenny Marks <kmarks2013@gmail.com>
2019-08-02 13:34:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element| element.capitalize
+  planeteer_calls.map do |element| element.capitalize "!"
   puts "#{element}!"
   end
 end
